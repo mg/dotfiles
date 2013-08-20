164fc5287bd9ef161ea25d4bfd1175c08d516739
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2013-08-20 21:56:10 +0000

    Fish prompt

--- config.fish
+++ config.fish
@@ -1,5 +1,5 @@
 # java
-set -xx JAVA_HOME usr/libexec/java_home
+set -x JAVA_HOME usr/libexec/java_home
 
 # hadoop
 set -x HADOOP_VERSION Hadoop2
@@ -37,4 +37,37 @@ function unmark
 end
 function marks
   ls -l $MARKPATH | cut -d' ' -f9-; and echo
+end
+
+# prompt
+set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑" 
+set -g __fish_git_prompt_char_upstream_behind "↓" 
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "+"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow 
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green
+
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  set_color normal
 end
